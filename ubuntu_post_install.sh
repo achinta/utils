43c4144c6085427ac2573e936b82b05ae06f1f55
achinta <achinta@users.noreply.github.com>
2015-02-13 15:30:40 +0530

    installing only salt

--- ubuntu_post_install.sh
+++ ubuntu_post_install.sh
@@ -1,33 +1,36 @@
-#install google chrome from command line 
-#Follow steps from http://www.liberiangeek.net/2011/12/install-google-chrome-using-apt-get-in-ubuntu-11-10-oneiric-ocelot/
+sudo apt-get update
 
-#sudo apt-get update
+#install saltstack
+sudo apt-get -y install python-software-properties
+sudo add-apt-repository -y ppa:saltstack/salt
+sudo apt-get update
+sudo apt-get -y install salt-master salt-minion
 
 #utils
-sudo apt-get -y install curl 
+#sudo apt-get -y install curl 
 
 #version control
-sudo apt-get -y install subversion git
+#sudo apt-get -y install subversion git
 
 #web servers
-sudo apt-get -y install apache2 libapache2-mod-wsgi nginx
+#sudo apt-get -y install apache2 libapache2-mod-wsgi nginx
 
 #media player
 #sudo apt-get install vlc
 
 #postgresql
-sudo apt-get -y install postgresql postgresql-contrib libpq-dev 
+#sudo apt-get -y install postgresql postgresql-contrib libpq-dev 
 
 #postgis TODO check version
-sudo apt-get -y install postgresql-9.3-postgis-2.1 
+#sudo apt-get -y install postgresql-9.3-postgis-2.1 
 
 #python
-sudo apt-get -y install python-pip python-psycopg2 python-dev
-sudo pip install virtualenv
-sudo pip install virtualenvwrapper
+#sudo apt-get -y install python-pip python-psycopg2 python-dev
+#sudo pip install virtualenv
+#sudo pip install virtualenvwrapper
 
 #google chrome
-sudo apt-get -y install chromium-browser
+#sudo apt-get -y install chromium-browser
 #wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 #sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 #sudo apt-get install google-chrome-stable
