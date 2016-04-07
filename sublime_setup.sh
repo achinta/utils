8e6dbdf0e4ebf3fe5f23686fbecb11795433346a
achinta <achinta@users.noreply.github.com>
2016-04-07 12:08:02 +0530

    added xml indent

--- sublime_setup.sh
+++ sublime_setup.sh
@@ -1,3 +1,4 @@
 #installing plugins
 git clone git://github.com/kemayo/sublime-text-2-git.git Git
 git clone git://github.com/jisaacks/GitGutter.git
+git clone https://github.com/alek-sys/sublimetext_indentxml.git
