7a39afbffac9d2e53ea2babe6bc244aa41364b34
achinta <achinta@users.noreply.github.com>
2016-05-21 16:23:45 +0530

    Update sublime_setup.sh

--- sublime_setup.sh
+++ sublime_setup.sh
@@ -1,5 +1,9 @@
 #installing plugins
 git clone https://github.com/DamnWidget/anaconda   #python code completion
-https://github.com/SublimeGit/SublimeGit.git
+git clone https://github.com/SublimeGit/SublimeGit.git
 git clone git://github.com/jisaacks/GitGutter.git
 git clone https://github.com/alek-sys/sublimetext_indentxml.git
+
+git clone https://github.com/guillermooo/Vintageous.git
+./bin/build.sh
+
