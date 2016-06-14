85cacb4ddfa2333fbd11a258d94d8e6f6aca238c
achinta <achinta@users.noreply.github.com>
2016-06-14 11:17:08 +0530

    Update sublime_setup.sh

--- sublime_setup.sh
+++ sublime_setup.sh
@@ -3,7 +3,7 @@ git clone https://github.com/DamnWidget/anaconda   #python code completion
 git clone https://github.com/SublimeGit/SublimeGit.git
 git clone git://github.com/jisaacks/GitGutter.git
 git clone https://github.com/alek-sys/sublimetext_indentxml.git
-git clone https://github.com/dzhibas/SublimePrettyJson.git
+#git clone https://github.com/dzhibas/SublimePrettyJson.git
 git clone https://github.com/akalongman/sublimetext-codeformatter.git CodeFormatter
 
 git clone https://github.com/guillermooo/Vintageous.git
